268158921eddba9605e3f64cb99cb69f2e697fff
marissecharette <124939657+marissecharette@users.noreply.github.com>
2023-03-28 19:59:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,68 +1,37 @@
-const input = document.querySelector('.entered.todo');
-const addButton = document.querySelector('.add-todo');
-const tasks = document.querySelector('.tasks');
-const form = document.querySelector('form');
+// Array that is provided by the user
+const todos = [];
+const todoList = document.querySelector(".todo-items");
+const todoForm = document.querySelector(".list")
+const toName = document.querySelector("#todo-name")
 
-// Prevent form submission when clicking on the "Add" button. NOT WORKING?
-form.addEventListener('submit', (e) => {
+
+function addTodoItem(e) {
   e.preventDefault();
-  addTodo();
-})
+  const todoName = toName.value;
+  todos.push(todoName);
+  renderList(todos, todoList);
+  todoForm.reset();
+}
 
-// Prevent user from submitting if there is no text submitted in the todo field. NOT WORKING..?
-addButton.addEventListener('keyup', () => {
-  if (input.value.trim() !== '') {
-    addButton.classList.add('active')
+function renderList(items, itemsList) {
+
+  while (itemsList.firstChild) {
+    itemsList.removeChild(itemsList.firstChild);
   }
-  else {
-    addButton.classList.remove('active')
+  for (let i = 0; i < items.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = items[i];
+    if (i === items.length - 1) {
+      listItem.classList.add("new-item-annimate");
+    }
+
+    itemsList.appendChild(listItem);
   }
-})
+}
 
-// Add new todo to list. NOT WORKINGGGGG
-addButton.addEventListener('click', () => {
-  if(input.value.trim() !== '') {
-    
-    let newItem = document.createElement('div');
-    newItem.classList.add('item');
-    newItem.innerHTML = `
-    <div class="checkbox-item">
-    <input type="checkbox" id="checkbox-item" />
-    <label class="checkmark" for="checkbox-item">${input.value}</label>
-    </div>
-    <div class="item-button">
-      <div class="edit-icon">
-        <i class="fas fa-solid fa-pen"></i>
-      </div>
-      <div class="delete-icon">
-        <i class="fas fa-solid fa-trash"></i>
-      </div>
-    </div>
-    `
-    tasks.appendChild(newItem);
-    input.value = '';
-  }
-  else {
-    alert("Please enter a task.");
-  }
-})
+todoForm.addEventListener("submit", addTodoItem);
 
-// Remove todo from list
-tasks.addEventListener('click', (e) => {
-  if (e.target.classList.contains('fa-solid')) {
-    e.target.parentElement.parentElement.remove();
-  }
-})
-
-// Mark item as completed
-tasks.addEventListener('click', (e) => {
-  if (e.target.classList.contains('fa-pen')) {
-    e.target.parentElement.parentElement.classList.toggle('completed');
-  }
-})
-
-
-
+renderList(todos, todoList);
 
 // Whenever a list name is changed in the main content section, this change is reflected in the sidebar.
 // list-title = firstInput
