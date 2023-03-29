5c88a32d3b7c826edf4ce96a0683da1b5bd187a9
marissecharette <124939657+marissecharette@users.noreply.github.com>
2023-03-28 22:59:01 -0400

    Update main.js
    Items don't stay deleted. As soon as another todo is added, the previously deleted todos come back.

--- main.js
+++ main.js
@@ -4,15 +4,17 @@ const todoList = document.querySelector(".todo-items");
 const todoForm = document.querySelector(".list")
 const toName = document.querySelector("#todo-name")
 
-
+// Prevents default form submission
 function addTodoItem(e) {
   e.preventDefault();
+
   const todoName = toName.value;
   todos.push(todoName);
   renderList(todos, todoList);
   todoForm.reset();
 }
 
+// Adds user inputted text as a todo in the list
 function renderList(items, itemsList) {
 
   while (itemsList.firstChild) {
@@ -21,6 +23,19 @@ function renderList(items, itemsList) {
   for (let i = 0; i < items.length; i++) {
     const listItem = document.createElement("li");
     listItem.textContent = items[i];
+
+    const btn = document.createElement("button");
+    btn.textContent = "Delete";
+    btn.classList.add("delete-button");
+    btn.dataset.index = i;
+    btn.onclick = function () {
+      const itemIndex = this.dataset.index; // Gets the index of the current item
+      const listItem = this.parentNode; // Gets the parent node
+      listItem.parentNode.removeChild(listItem); // Removes the parent node
+    };
+    
+    listItem.appendChild(btn); // Appends the button
+
     if (i === items.length - 1) {
       listItem.classList.add("new-item-annimate");
     }
